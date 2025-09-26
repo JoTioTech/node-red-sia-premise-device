951167cbf40253ee4b9494709000b1244cd06fab
Havra <krystof@havrak.xyz>
2025-09-26 14:28:49 +0200

    Got SIA-server config node to work

--- SIA_device.js
+++ SIA_device.js
@@ -1,29 +1,25 @@
 const clientModule = __importDefault(require('./src/SIA_device.js'));
 
-
 module.exports = function (RED) {
-
-
-	function ParserNode(config) {
+	function SIADeviceNode(config) {
 		RED.nodes.createNode(this, config);
 
-		this.serverConfig = RED.nodes.getNode(config.server);
-		const siaModule = new clientModule.default(serverConfig);
+		const siaSever = RED.nodes.getNode(config.server)
+		const siaModule = new clientModule.default(siaSever);
 		siaModule.connect();
 
 		this.on('input', message => {
 			parsedPayload = message.payload;
 
+			// Const parser = new parserModule.default(message.statusSchema, parserConfig);
+			// message.payload.outJson = parser.processPayload(parsedPayload);
 
-				// const parser = new parserModule.default(message.statusSchema, parserConfig);
-				// message.payload.outJson = parser.processPayload(parsedPayload);
-
-
-			if(parsedPayload === undefined){
+			if (parsedPayload === undefined) {
 				this.send(message);
 				return;
 			}
-			if(message.payload.parsingError == true){
+
+			if (message.payload.parsingError == true) {
 				this.send(message);
 				return;
 			}
@@ -32,12 +28,6 @@ module.exports = function (RED) {
 		});
 	}
 
-	RED.nodes.registerType('SIA-device', ParserNode);
-	RED.nodes.registerType('SIA-server', require('./sia-server.js'));
-
+	RED.nodes.registerType('SIA-device', SIADeviceNode);
 
 };
-
-
-
-
--- SIA_server.js
+++ SIA_server.js
@@ -1,14 +1,119 @@
-module.exports = function(RED) {
-    function SIAServerNode(config) {
-        RED.nodes.createNode(this, config);
-        // Store the configuration values
-        this.name = config.name;
-        this.receiverIP = config.receiverIP;
-        this.receiverPort = config.receiverPort;
-        this.siaAccount = config.siaAccount;
-        this.siaPassword = config.siaPassword;
-        this.encryptionEnabled = config.encryptionEnabled;
-        this.passwordInHex = config.passwordInHex;
-    }
-    RED.nodes.registerType("SIA-server", SIAServerNode);
-};
+/**
+ Copyright (c) since the year 2016 Klaus Landsdorf (http://plus4nodered.com/)
+ Copyright 2016 - Jason D. Harper, Argonne National Laboratory
+ Copyright 2015,2016 - Mika Karaila, Valmet Automation Inc.
+ All rights reserved.
+ node-red-contrib-modbus
+
+ @author <a href="mailto:klaus.landsdorf@bianco-royal.de">Klaus Landsdorf</a> (Bianco Royal)
+ */
+
+/**
+ * Modbus connection node.
+ * @module NodeRedModbusClient
+ *
+ * @param RED
+ */
+module.exports = function (RED) {
+  'use strict'
+  // SOURCE-MAP-REQUIRED
+  // const mbBasics = require('./modbus-basics')
+  // const coreModbusClient = require('./core/SIA-server-core')
+  // const coreModbusQueue = require('./core/modbus-queue-core')
+  // const internalDebugLog = require('debug')('contribModbus:config:clienta')
+  // const _ = require('underscore')
+
+  function SIAServer (config) {
+    RED.nodes.createNode(this, config)
+
+		this.serverHost = config.serverHost;
+		this.serverPort = parseInt(config.serverPort) || 5093;
+		this.siaAccount = config.siaAccount;
+		this.siaPassword = config.siaPassword;
+		this.encryptionEnabled = config.encryptionEnabled || false;
+		this.passwordInHex  = config.passwordInHex || false;
+
+    // create an empty modbus clienta
+    // const ModbusRTU = require('@openp4nr/modbus-serial')
+
+    // const unlimitedListeners = 0
+    // const minCommandDelayMilliseconds = 1
+    // const defaultUnitId = 1
+    // const defaultTcpUnitId = 0
+    // const serialConnectionDelayTimeMS = 500
+    // const timeoutTimeMS = 1000
+    // const reconnectTimeMS = 2000
+    // const logHintText = ' Get More About It By Logging'
+    // const serialAsciiResponseStartDelimiter = '0x3A'
+    //
+    // this.clientatype = config.clientatype
+    //
+    // if (config.parallelUnitIdsAllowed === undefined) {
+    //   this.bufferCommands = true
+    // } else {
+    //   this.bufferCommands = config.bufferCommands
+    // }
+    //
+    // this.queueLogEnabled = config.queueLogEnabled
+    // this.stateLogEnabled = config.stateLogEnabled
+    // this.failureLogEnabled = config.failureLogEnabled
+    //
+    // this.tcpHost = config.tcpHost
+    // this.tcpPort = parseInt(config.tcpPort) || 502
+    // this.tcpType = config.tcpType
+    //
+
+
+    // this.serialPort = config.serialPort
+    // this.serialBaudrate = config.serialBaudrate
+    // this.serialDatabits = config.serialDatabits
+    // this.serialStopbits = config.serialStopbits
+    // this.serialParity = config.serialParity
+    // this.serialType = config.serialType
+    // this.serialConnectionDelay = parseInt(config.serialConnectionDelay) || serialConnectionDelayTimeMS
+    // this.serialAsciiResponseStartDelimiter = config.serialAsciiResponseStartDelimiter || serialAsciiResponseStartDelimiter
+    //
+    // this.unit_id = parseInt(config.unit_id)
+    // this.commandDelay = parseInt(config.commandDelay) || minCommandDelayMilliseconds
+    // this.clientaTimeout = parseInt(config.clientaTimeout) || timeoutTimeMS
+    // this.reconnectTimeout = parseInt(config.reconnectTimeout) || reconnectTimeMS
+    // this.reconnectOnTimeout = config.reconnectOnTimeout
+    //
+    // if (config.parallelUnitIdsAllowed === undefined) {
+    //   this.parallelUnitIdsAllowed = true
+    // } else {
+    //   this.parallelUnitIdsAllowed = config.parallelUnitIdsAllowed
+    // }
+    //
+    // this.showErrors = config.showErrors
+    // this.showWarnings = config.showWarnings
+    // this.showLogs = config.showLogs
+    //
+    // const node = this
+    // node.isFirstInitOfConnection = true
+    // node.closingModbus = false
+    // node.clienta = null
+    // node.bufferCommandList = new Map()
+    // node.sendingAllowed = new Map()
+    // node.unitSendingAllowed = []
+    // node.messageAllowedStates = coreModbusClient.messageAllowedStates
+    // node.serverInfo = ''
+    //
+    // node.stateMachine = null
+    // node.stateService = null
+    // node.stateMachine = coreModbusClient.createStateMachineService()
+    // node.actualServiceState = node.stateMachine.initialState
+    // node.actualServiceStateBefore = node.actualServiceState
+    // node.stateService = coreModbusClient.startStateService(node.stateMachine)
+    // node.reconnectTimeoutId = 0
+    // node.serialSendingAllowed = false
+    // node.internalDebugLog = internalDebugLog
+    //
+    // coreModbusQueue.queueSerialLockCommand(node)
+    //
+  }
+
+  RED.nodes.registerType('SIA-server', SIAServer)
+
+  /* istanbul ignore next */
+}
